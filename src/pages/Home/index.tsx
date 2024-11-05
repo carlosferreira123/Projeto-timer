fb31a270a0c14f9a1a9bd8716b2e5a7309f4c87c
Carlos Alisson <carlosalisson01@gmail.com>
2024-11-04 22:10:07 -0300

    a

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,19 +3,11 @@ import { CountdowContainer, FormContainer, HomeContainer, MinutesAmountInput, Se
 import { useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
-import { useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { differenceInSeconds } from 'date-fns'
 import { NewCycleForm } from "./NewCycleForm";
+import { Countdown } from "./Countdown";
 
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5).max(60, 'O valor deve ser entre 5 e 60')
-})
-
-
-
-
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 interface Cycle {
   id: string;
@@ -25,62 +17,32 @@ interface Cycle {
   interruptedDate?: Date
   finisheDate?: Date
 }
+interface CyclesContextType {
+  activeCycle: Cycle | undefined;
+  activeCycleId: string | null
+  markCurrentCycleAsFinished: () => void
+}
+
+const CyclesContext = createContext({} as CyclesContextType)
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>( {
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    }
-  })
+  
   
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
-
-
-  useEffect(() => {
-    let interval: number
-    
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds (
-          new Date(), 
-          activeCycle.startDate,
-          
-        )  
-        
-        if (secondsDifference > totalSeconds) {
-         setCycles((state) => 
-          state.map((cycle) => {
-          if (cycle.id === activeCycleId) {
-            return { ...cycle, finisheDate: new Date()}
-          } else {
-            return cycle
-          }
-         }),
-        ) 
-          setAmountSecondsPassed(totalSeconds)
-
-          clearInterval(interval)  
-         } else {
-          setAmountSecondsPassed(secondsDifference)
-        }
-
-      }, 1000)
-    }
-    return () => {
-      clearInterval(interval)
-    }
-  }, [activeCycle, totalSeconds, activeCycleId])
-
-
-
+  function markCurrentCycleAsFinished() {
+    setCycles((state) => 
+      state.map((cycle) => {
+      if (cycle.id === activeCycleId) {
+        return { ...cycle, finisheDate: new Date()}
+      } else {
+        return cycle
+      }
+     }),
+    ) 
+  }
   
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());  
@@ -117,20 +79,7 @@ export function Home() {
   }
   
 
-const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
-const minutesAmount = Math.floor(currentSeconds / 60)
-const secondsAmount = currentSeconds % 60
-
-const minutes = String(minutesAmount).padStart(2, '0')
-const seconds = String(secondsAmount).padStart(2, '0')
-
-useEffect(() => {
-  if (activeCycle){
-    document.title = `${minutes}:${seconds}`
-  }
-  
-}, [minutes, seconds, activeCycle])
 
 const task = watch('task')
 const isSubmitDisabled = !task; 
@@ -138,9 +87,13 @@ const isSubmitDisabled = !task;
   return (
     <HomeContainer>
        <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <NewCycleForm/>
-        <CountdowContainer/>
-         
+        <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}>
+         <NewCycleForm/>
+         <Countdown  
+          activeCycle={activeCycle} 
+          setCycles={setCycles} 
+          activeCycleId={activeCycleId}/>
+        </CyclesContext.Provider>
          
 
          { activeCycle ? (
