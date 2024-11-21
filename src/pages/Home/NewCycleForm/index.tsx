04713b978e7082c23abbd2745714adf553e7e207
Carlos Alisson <carlosalisson01@gmail.com>
2024-11-20 21:32:12 -0300

    Atualização

--- src/pages/Home/NewCycleForm/index.tsx
+++ src/pages/Home/NewCycleForm/index.tsx
@@ -1,25 +1,15 @@
-import { useForm } from "react-hook-form";
+import { useFormContext } from "react-hook-form";
+import { CyclesContext } from "..";
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
-import * as zod from 'zod';
-import { zodResolver } from "@hookform/resolvers/zod";
 
-const newCycleFormValidationSchema = zod.object({
-    task: zod.string().min(1, 'Informe a tarefa'),
-    minutesAmount: zod.number().min(5).max(60, 'O valor deve ser entre 5 e 60')
-  })
-  
- type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
-  
+import { useContext } from "react";
+
+
 
 export function NewCycleForm() {  
-    
-    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>( {
-        resolver: zodResolver(newCycleFormValidationSchema),
-        defaultValues: {
-          task: '',
-          minutesAmount: 0,
-        }
-      })
+   const { activeCycle } = useContext(CyclesContext)
+   const { register } = useFormContext()
+   
     
     
     return (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,10 @@
 import { HandPalm, Play } from "phosphor-react";
-import { CountdowContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, StopCountdownButton, TaskInput } from "./styles";
-import { useForm } from 'react-hook-form'
+import { HomeContainer, StartCountdownButton, StopCountdownButton, } from "./styles";
+import { FormProvider, useForm } from 'react-hook-form'
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from 'zod'
-import { createContext, useEffect, useState } from "react";
-import { differenceInSeconds } from 'date-fns'
+import { createContext, useState } from "react";
+
 import { NewCycleForm } from "./NewCycleForm";
 import { Countdown } from "./Countdown";
 
@@ -21,17 +21,43 @@ interface CyclesContextType {
   activeCycle: Cycle | undefined;
   activeCycleId: string | null
   markCurrentCycleAsFinished: () => void
+  amountSecondsPassed: number
+  setSecondsPassed: (seconds: number) => void
+  
 }
 
-const CyclesContext = createContext({} as CyclesContextType)
+export const CyclesContext = createContext({} as CyclesContextType)
+
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60, 'O valor deve ser entre 5 e 60')
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
   
+  const newCycleForm = useForm<NewCycleFormData>( {
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  })
+
+  const { handleSubmit, watch, reset } = newCycleForm
   
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+ 
   function markCurrentCycleAsFinished() {
     setCycles((state) => 
       state.map((cycle) => {
@@ -87,12 +113,19 @@ const isSubmitDisabled = !task;
   return (
     <HomeContainer>
        <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
-        <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}>
-         <NewCycleForm/>
-         <Countdown  
-          activeCycle={activeCycle} 
-          setCycles={setCycles} 
-          activeCycleId={activeCycleId}/>
+        <CyclesContext.Provider 
+          value={{
+             activeCycle, 
+             activeCycleId, 
+             markCurrentCycleAsFinished,
+             amountSecondsPassed, 
+             setSecondsPassed,
+             }}>
+         
+         <FormProvider {...newCycleForm}>
+          <NewCycleForm/>
+         </FormProvider>
+         <Countdown/>   
         </CyclesContext.Provider>
          
 
